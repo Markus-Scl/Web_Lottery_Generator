e22b897f54125e84161c04d49c6ab8890def6c88
Markus-Scl <76035547+Markus-Scl@users.noreply.github.com>
2023-01-08 10:43:46 +0100

    added fire work animation

--- client/lottery_wep_page/src/app/main-page/lottery-draw-page/lottery-draw-page.component.ts
+++ client/lottery_wep_page/src/app/main-page/lottery-draw-page/lottery-draw-page.component.ts
@@ -15,6 +15,7 @@ export class LotteryDrawPageComponent {
     twelveArray: number [] = [];
 
     seeHeadline = false;
+    lotteryDrawingRunning = false;
 
     prefillArrays(fiftyNumbers: number[], tweveNumbers: number[]){
       for(let i = 1; i < 51; i++){
@@ -25,43 +26,55 @@ export class LotteryDrawPageComponent {
       }
     }
 
-    
-
   randomIntFromInterval(min: number, max: number) { 
       return Math.floor(Math.random() * (max - min + 1) + min)
   }
 
   async drawNumbersFiveFromFifty(){
+    if(!this.lotteryDrawingRunning){
 
-    let fiftyNumbers: number[] = [];
-    let twelveNumbers: number[] = [];
+      this.lotteryDrawingRunning = true;
+      this.seeHeadline = false;
 
-    this.prefillArrays(fiftyNumbers, twelveNumbers);
+      let fiftyNumbers: number[] = [];
+      let twelveNumbers: number[] = [];
+  
+      this.fiveOutOfFifty = [];
+      this.twoOutOfTwelve = [];
+  
+      this.displayedFiveNumbers = [];
+      this.displayedTwoNumbers = [];
+  
+      this.prefillArrays(fiftyNumbers, twelveNumbers);
+  
+      for(let i = 0; i < 5; i++){
+        let idx = this.randomIntFromInterval(0, fiftyNumbers.length - 1);
+        this.fiveOutOfFifty.push(fiftyNumbers[idx]);
+        fiftyNumbers.splice(idx, 1);
+      }
+      this.fiveOutOfFifty = this.fiveOutOfFifty.sort((n1, n2) => n1 - n2);
+  
+      for(let number of this.fiveOutOfFifty){
+        this.displayedFiveNumbers.push(number);
+        await new Promise(f => setTimeout(f, 800));
+      }
+      
+  
+      for(let i = 0; i < 2; i++){
+        let idx = this.randomIntFromInterval(0, twelveNumbers.length - 1);
+        this.twoOutOfTwelve.push(twelveNumbers[idx]);
+        twelveNumbers.splice(idx, 1);
+      }
 
-    for(let i = 0; i < 5; i++){
-      let idx = this.randomIntFromInterval(0, fiftyNumbers.length - 1);
-      this.fiveOutOfFifty.push(fiftyNumbers[idx]);
-      fiftyNumbers.splice(idx, 1);
+      this.twoOutOfTwelve = this.twoOutOfTwelve.sort((n1, n2) => n1 - n2);
+  
+      for(let number of this.twoOutOfTwelve){
+        this.displayedTwoNumbers.push(number);
+        await new Promise(f => setTimeout(f, 800));
+      }
+
+      this.seeHeadline = true;
+      this.lotteryDrawingRunning = false;
     }
-    this.fiveOutOfFifty = this.fiveOutOfFifty.sort((n1, n2) => n1 - n2);
-
-    for(let number of this.fiveOutOfFifty){
-      this.displayedFiveNumbers.push(number);
-      await new Promise(f => setTimeout(f, 800));
-    }
-
-    for(let i = 0; i < 2; i++){
-      let idx = this.randomIntFromInterval(0, twelveNumbers.length - 1);
-      this.twoOutOfTwelve.push(twelveNumbers[idx]);
-      twelveNumbers.splice(idx, 1);
-    }
-    this.twoOutOfTwelve = this.twoOutOfTwelve.sort((n1, n2) => n1 - n2);
-
-    for(let number of this.twoOutOfTwelve){
-      this.displayedTwoNumbers.push(number);
-      await new Promise(f => setTimeout(f, 800));
-    }
-
-    this.seeHeadline = true;
   }
 }
